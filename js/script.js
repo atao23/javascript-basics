eca65cf3517de3f2a37f8626150fd254dc10824d
iGuest <atao@uw.edu>
2014-10-21 10:05:51 -0700

    added audio and other stuff to JS

--- js/script.js
+++ js/script.js
@@ -43,4 +43,21 @@ kitty.alt = 'picture of a cat';
 //add this to the body of the page
 document.body.appendChild(kitty);
 
+//function for onPuppyClick
+function onPuppyClick() {
+	//alert('you clicked my puppy!');
+	document.getElementById('bark').play();
+} //onPuppyClick()
 
+//selecting the puppy-pic element
+var pup = document.getElementById('puppy-pic');
+
+if (pup.addEventListener)
+	pup.addEventListener('click', onPuppyClick);
+else if (pup.attachEvent)
+	pup.attachEvent('click', onPuppyClick);
+
+document.addEventListener('DOMContentLoaded', function(){
+	//add event listeners for elements
+	//manipulate the DOM
+});
